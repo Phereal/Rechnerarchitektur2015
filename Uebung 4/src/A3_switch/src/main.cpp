84b82a9d5b080d6bbf6255c5f26073afd7536515
Daniel Tauritis <tauritis@uni-bremen.de>
2015-12-12 14:18:23 +0100

    main für aufgabe 3 angepasst und leere dateien gelöscht

--- Uebung 4/src/A3_switch/src/main.cpp
+++ Uebung 4/src/A3_switch/src/main.cpp
@@ -1,1 +1,12 @@
 #include "packet.h"
+#include <iostream>
+#include <string>
+#include <cstdint>
+#include <exception>
+#include <systemc.h>
+using namespace std;
+
+int sc_main()
+    {
+    return 0;
+    }
