80295757b27690ac042828fc1634b9353e500a64
Daniel Tauritis <tauritis@uni-bremen.de>
2015-12-12 17:06:24 +0100

    packet berücksichtigt nun flag in allen funktionen und so

--- Uebung 4/RAuES_U4_A3/src/packet.h
+++ Uebung 4/RAuES_U4_A3/src/packet.h
@@ -14,25 +14,26 @@ struct packet
 
     // must implement '=' '==' and '<<' (nice would be 'sc_trace')
 
-    packet(int a, int p) :
-	addr(a), payload(p)
+    packet(int a, int p, bool f) :
+	addr(a), payload(p), flag(f)
 	{
 	}
 
     packet() :
-	addr(0), payload(0)
+	addr(0), payload(0), flag(false)
 	{
 	}
 
     inline bool operator==(const packet &rhs) const
 	{
-	return (rhs.addr == addr && rhs.payload == payload);
+	return (rhs.addr == addr && rhs.payload == payload && rhs.flag == flag);
 	}
 
     inline packet &operator=(const packet &rhs)
 	{
 	addr = rhs.addr;
 	payload = rhs.payload;
+	flag = rhs.flag;
 	return *this;
 	}
 
@@ -41,11 +42,12 @@ struct packet
 	{
 	sc_trace(tf, v.addr, NAME + ".addr");
 	sc_trace(tf, v.payload, NAME + ".payload");
+	sc_trace(tf, v.flag, NAME + ".flag");
 	}
 
     inline friend ostream &operator<<(ostream &os, packet const &v)
 	{
-	os << "(" << v.addr << "," << v.payload << ")";
+	os << "(" << v.addr << "," << v.payload << "," << v.flag << ")";
 	return os;
 	}
     };
