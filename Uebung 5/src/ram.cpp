9803e00b2fe14ed2d1e4002ca75ffe4a8b065b32
Stefan Fuhrmann <Stefan.Fuhrmann@phereal.net>
2016-01-13 13:12:15 +0100

    Ram erweitert!

--- Uebung 5/src/ram.cpp
+++ Uebung 5/src/ram.cpp
@@ -23,22 +23,21 @@
 
 using namespace std;
 
-ram::ram(sc_module_name name, uint8_t id, uint32_t bufferSize) :
-    module(name, id, bufferSize)
+ram::ram(sc_module_name name, uint8_t id, uint32_t bufferSize) : module(name, id, bufferSize)
 {
-  if(initialize == false)
-  {
+  if(initialize == false) {
     init();
     initialize = true;
   }
   pakethandler();
+  //TODO Die Clock des RAM soll 2 mal langsamer sein, als die anderen Clocks, diese andere Clock muss aus der Main übergeben werden!
   sensitive << clk.pos();
 
   //SC_HAS_PROCESS(ram);
 }
 //Überschreiben der process-Methode der Elternklasse Module
-bool ram::process(paket &pkg)
-{
+//TODO Wird immer nur aufgerufen, wenn ein Paket auch eigeht, somit kann momentan nur bei eingehenden Paketen gesendet werden!
+bool ram::process(paket &pkg){
   enable = false;
   //Paket einlesen
   i_id = pkg.id;
@@ -57,22 +56,19 @@ bool ram::process(paket &pkg)
   pkg.xpos = o_xpos;
   pkg.ypos = o_ypos;
   pkg.color = o_color;
-  return enable;
+        return enable;
 }
 
-void ram::pakethandler()
-{
+void ram::pakethandler() {
   //Es werden nur OPCodes behandelt, die auch durch das RAM-Modul verarbeitet werden können.
-  switch(i_opcode)
-  {
-    case 0x00:
-      break; //[emp]
-    case 0x01:
-      break; //[exe]
-    case 0x02:
-      break; //[fin]
-    case 0x03:
-      break; //[c_req]
+  //Die Behandlung der gesendeten Module in den SendeBuffer wird in der Oberklasse durchgeführt.
+  //Hier werden die Pakete direkt verarbeitet und dann über den SendeBuffer der Elternklasse
+  //weiterverarbeitet.
+  switch(i_opcode){
+    case 0x00: break;//[emp]
+    case 0x01: break;//[exe]
+    case 0x02: break;//[fin]
+    case 0x03: break;//[c_req]
     case 0x04: //[r_req]
       o_id = 0;
       o_opcode = 0x07; //[ir_pay]
@@ -80,16 +76,15 @@ void ram::pakethandler()
       o_receiver = i_sender;
       o_xpos = i_xpos;
       o_ypos = i_ypos;
-      o_color = readPixel(i_xpos, i_ypos);
+      o_color = readPixel(i_xpos,i_ypos);
       enable = true;
-    case 0x05:
-      break; //[ack]
-    case 0x06:
-      break; //[ic_pay]
-    case 0x07:
-      break; //[ir_pay]
+      break;
+    case 0x05: break;//[ack]
+    case 0x06: break;//[ic_pay]
+    case 0x07: break;//[ir_pay]
     case 0x08: //[o_pay]
-      writePixel(i_xpos, i_ypos, i_color);
+      writePixel(i_xpos,i_ypos,i_color);
+      break;
     case 0x09: //[rfi]
       readPGM();
       o_id = 0;
@@ -100,8 +95,8 @@ void ram::pakethandler()
       o_ypos = 0;
       o_color = 0;
       enable = true;
-    case 0x0A:
-      break; //[rff]
+      break;
+    case 0x0A: break;//[rff]
     case 0x0B: //[wfi]
       writePGM();
       o_id = 0;
@@ -112,104 +107,96 @@ void ram::pakethandler()
       o_ypos = 0;
       o_color = 0;
       enable = true;
-    case 0x0C:
-      break; //[wff]
+      break;
+    case 0x0C: break;//[wff]
     case 0x0D: //[nxt]
       o_id = 0;
       o_opcode = 0x0E; //[nxa]
       o_sender = i_receiver;
       o_receiver = i_sender;
-      nxtPixel(&o_xpos, &o_ypos);
+      nxtPixel(&o_xpos,&o_ypos);
       o_color = 0;
       enable = true;
-    case 0x0E:
-      break; //[nxa]
-    default:
       break;
+    case 0x0E: break;//[nxa]
+    case 0x0F: break;//[end]
+    case 0x10: //[brd]
+      o_id = 0;
+      o_opcode = 0x11; //[brr]
+      o_sender = i_receiver;
+      o_receiver = i_sender;
+      o_xpos = width;
+      o_ypos = height;
+      o_color = 0;
+      enable = true;
+      break;
+    case 0x11: break; //[brr]
+    default: break;
   }
 }
 
 // Wenn alle Pixel berechnet wurden werden die Koordinaten X=-1 und Y=-1 zurückgegeben.
-void ram::nxtPixel(uint32_t *xpos, uint32_t *ypos)
-{
-  for(unsigned int i = 0; i < (*ypos); i++)
-  {
-    for(unsigned int j = 0; j < (*xpos); j++)
-    {
-      if(nxt_map[i][j] == false)
-      {
+void ram::nxtPixel(uint32_t *xpos, uint32_t *ypos){
+  for (unsigned int i = 0; i < (*ypos); i++){
+    for (unsigned int j = 0; j < (*xpos); j++){
+      if(nxt_map[i][j] == false){
         (*xpos) = i;
         (*ypos) = j;
         break;
       }
-      else
-      {
+      else {
         // TODO Benachrichtigung des Gateway-Moduls, dass alle Pixel berechnet wurden.
       }
     }
   }
 }
-
 // Initialisierung aller Vektoren mit Initialwerten
-void ram::init()
-{
-  for(int i = 0; i < height; i++)
-  {
-    for(int j = 0; j < width; j++)
-    {
+void ram::init(){
+  for (int i = 0; i < height; i++){
+    for (int j = 0; j < width; j++){
       in_image[i][j] = 0;
       out_image[i][j] = 0;
       nxt_map[i][j] = false;
     }
   }
 }
-
 // Der Wert eines Pixels wird zurückgegeben
-unsigned char ram::readPixel(int xpos, int ypos)
-{
+unsigned char ram::readPixel(int xpos, int ypos) {
   return in_image[xpos][ypos];
 }
-
 // Ein Pixel im Zielbildvektor wird geschrieben mit Fehlerbehandlung für maximale Bildtiefe
-void ram::writePixel(int xpos, int ypos, unsigned char data)
-{
-  if(data < 0)
-  {
+void ram::writePixel(int xpos, int ypos, unsigned char data){
+  if(data < 0){
     data = 0;
   }
-  if(data > depth)
-  {
+  if(data > depth){
     data = depth;
   }
   out_image[xpos][ypos] = data;
   nxt_map[xpos][ypos] = true;
 }
-
 //void read_pgm(std::string filename, int& xpos, int& ypos, int& color_depth, pgm_image& image);
 // Das PGM-Bild wird über die Klasse pgm.h eingelesen
-void ram::readPGM()
-{
-  read_pgm(infile, width, height, depth, in_image);
-  cout << "read" << endl;
-  /*    for (int i = 0; i < heigth; i++){
-   for (int j = 0; j < width; j++){
-   cout << to_string(in_image[i][j]) << " ";
-   }
-   cout << "\n";
-   }*/
-  cout << "read finished" << endl;
+void ram::readPGM() {
+    read_pgm(infile,width,height,depth,in_image);
+    cout << "read" << endl;
+/*    for (int i = 0; i < heigth; i++){
+      for (int j = 0; j < width; j++){
+        cout << to_string(in_image[i][j]) << " ";
+      }
+      cout << "\n";
+    }*/
+    cout << "read finished" << endl;
 }
-
 // Das PGM-Bild wird über die Klasse pgm.h geschrieben
-void ram::writePGM()
-{
-  write_pgm(outfile, width, height, out_image, depth);
+void ram::writePGM(){
+  write_pgm(outfile,width,height,out_image,depth);
   cout << "write" << endl;
-  /*  for (int i = 0; i < height; i++){
-   for (int j = 0; j < width; j++){
-   cout << to_string(in_image[i][j]) << " ";
-   }
-   cout << "\n";
-   }*/
+/*  for (int i = 0; i < height; i++){
+    for (int j = 0; j < width; j++){
+      cout << to_string(in_image[i][j]) << " ";
+    }
+    cout << "\n";
+  }*/
   cout << "write finished" << endl;
 }
